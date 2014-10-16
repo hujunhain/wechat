9bfab4260ec2438ba02c67e399da325d9b4bea64
hujunhain <hujunhain@gmail.com>
2014-10-16 10:23:33 +0800

    idea

---
+++ grails-app/services/wechat/UserService.groovy
@@ -0,0 +1,34 @@
+package wechat
+
+import chanjarster.weixin.bean.result.WxUser
+import grails.transaction.Transactional
+
+@Transactional
+class UserService {
+
+    def serviceMethod() {
+
+    }
+
+    def save(WxUser user) {
+
+//        Account.withTransaction { status ->
+//                 def source = Account.get(params.from)
+//                 def dest = Account.get(params.to)
+//                def amount = params.amount.toInteger()
+//                 if(source.active)
+//                     {
+//                         source.balance -= amount
+//                         if(dest.active) {
+//                             dest.amount += amount
+//                         } else { status.setRollbackOnly() }
+//                     }
+//        }
+        WxUser.withTransaction { status ->
+            println "status:" + status
+            user.save();
+            println "bbbbb  status:" + status
+        }
+
+    }
+}
