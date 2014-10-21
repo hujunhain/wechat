0b78e1af9499d41c9633961939be699371521833
hujunhain <hujunhain@gmail.com>
2014-10-21 15:52:01 +0800

    manager page

--- grails-app/controllers/com/hhpc/wechat/controller/WechatController.groovy
+++ grails-app/controllers/com/hhpc/wechat/controller/WechatController.groovy
@@ -151,7 +151,7 @@ class WechatController {
 
             router
                     .rule().async(false).rContent("\\d{1,2}").handler(new WxMsgMessageHandler()).end()
-                    .rule().async(false).rContent("1\\d{10}").msgType(WxConsts.XML_MSG_TEXT).handler(new WxWangMessageHandler()).end()
+                    .rule().async(false).rContent("1\\d{10}.*").msgType(WxConsts.XML_MSG_TEXT).handler(new WxWangMessageHandler()).end()
                     .rule().async(false).event(WxConsts.EVT_SUBSCRIBE).handler(new WxSubMessageHandler()).end()
                     .rule().async(false).event(WxConsts.EVT_UNSUBSCRIBE).handler(new WxUnSubMessageHandler()).end()
                     .rule().async(false).handler(new WxEchoMessageHandler(sb, WxConsts.XML_MSG_TEXT)).end()
@@ -212,7 +212,13 @@ class WechatController {
                 //获取 权限
 
                 def sms = "拨动祝福的琴弦，为您唱出最美妙的生日歌，点亮幸福的烛光，在您生日之际，（青岛啤酒）华海鹏城衷心的祝您生日快乐，身体健康！"
+
+                if(sendNum.length()>11){
+                    sms= sendNum.substring(11)
+                    sendNum= sendNum.substring(0,11)
+                }
                 def smsId = smsService.send(sendNum, sms)
+
                 println "smsId    :"+smsId
                // def status=smsService.status(smsId)
                 msg="短信已经发送"+"("+sendNum+")"
