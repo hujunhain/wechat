ea720cee10b4e8455f4d3870ccf78e522758bd62
hujunhain <hujunhain@gmail.com>
2014-11-03 15:34:05 +0800

    mp cp

--- grails-app/controllers/com/hhpc/wechat/controller/QiyeController.groovy
+++ grails-app/controllers/com/hhpc/wechat/controller/QiyeController.groovy
@@ -3,11 +3,11 @@ package com.hhpc.wechat.controller
 class QiyeController {
     def cPConfig
     def application
-    def grailsapplication
+    def wxCpService
     def index() {
         println "cPConfig::"+cPConfig
         println "application:"+application?.config?.qiye?.corpId
-        println "grailsapplication:"+grailsapplication?.config?.qiye?.corpId
+        println "grailsapplicationXXXX:"+wxCpService.wxCpConfigStorage.corpId
         render "abc"+cPConfig
     }
 }
