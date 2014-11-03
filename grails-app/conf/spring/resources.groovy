dcbeeb87f9601624814b8c3cb6bde161a74877d9
hujunhain <hujunhain@gmail.com>
2014-11-03 15:10:30 +0800

    mp cp

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -3,14 +3,14 @@ import me.chanjar.weixin.cp.api.WxCpInMemoryConfigStorage
 
 // Place your Spring DSL code here
 beans = {
-       def grailsApplication=new org.codehaus.groovy.grails.commons.DefaultGrailsApplication()
+     //  def grailsApplication=new org.codehaus.groovy.grails.commons.DefaultGrailsApplication()
 
     cPConfig(WxCpInMemoryConfigStorage) {
-        corpId:grailsApplication.config.qiye.corpId;      // 设置微信企业号的appid
-        corpSecret:grailsApplication.config.qiye.corpSecret;  // 设置微信企业号的app corpSecret
-        agentId:grailsApplication.config.qiye.agentId;     // 设置微信企业号应用ID
-        token:grailsApplication.config.qiye.token;       // 设置微信企业号应用的token
-        aesKey:grailsApplication.config.qiye.aesKey;      // 设置微信企业号应用的EncodingAESKey
+        corpId:application.config.qiye.corpId;      // 设置微信企业号的appid
+        corpSecret:application.config.qiye.corpSecret;  // 设置微信企业号的app corpSecret
+        agentId:application.config.qiye.agentId;     // 设置微信企业号应用ID
+        token:application.config.qiye.token;       // 设置微信企业号应用的token
+        aesKey:application.config.qiye.aesKey;      // 设置微信企业号应用的EncodingAESKey
     }
 
 
