1915b1aa6b92c3b732c1f70172c9e70efc0c4086
hujunhain <hujunhain@gmail.com>
2014-11-03 15:26:23 +0800

    mp cp

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -6,11 +6,11 @@ beans = {
      //  def grailsApplication=new org.codehaus.groovy.grails.commons.DefaultGrailsApplication()
 
     cPConfig(WxCpInMemoryConfigStorage) {
-        corpId:application.config.qiye.corpId;      // 设置微信企业号的appid
-        corpSecret:application.config.qiye.corpSecret;  // 设置微信企业号的app corpSecret
-        agentId:application.config.qiye.agentId;     // 设置微信企业号应用ID
-        token:application.config.qiye.token;       // 设置微信企业号应用的token
-        aesKey:application.config.qiye.aesKey;      // 设置微信企业号应用的EncodingAESKey
+        corpId=application.config.qiye.corpId;      // 设置微信企业号的appid
+        corpSecret=application.config.qiye.corpSecret;  // 设置微信企业号的app corpSecret
+        agentId=application.config.qiye.agentId;     // 设置微信企业号应用ID
+        token='hhpc'//application.config.qiye.token;       // 设置微信企业号应用的token
+        aesKey=application.config.qiye.aesKey;      // 设置微信企业号应用的EncodingAESKey
     }
 
 
