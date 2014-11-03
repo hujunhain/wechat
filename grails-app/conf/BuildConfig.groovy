712338255eff5e458e0811b8dd6f1ee1381fbc1c
hujunhain <hujunhain@gmail.com>
2014-11-03 12:01:41 +0800

    mp cp

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -68,15 +68,18 @@ grails.project.dependency.resolution = {
 
     plugins {
         // plugins for the build system only
-        build ":tomcat:7.0.55"
+       // build ":tomcat:7.0.55"
+
+        compile ":jetty:3.0.0"
 
         // plugins for the compile step
         compile ":scaffolding:2.1.2"
-        compile ':cache:1.1.7'
-        compile ":asset-pipeline:1.9.6"
+        compile ':cache:1.1.8'
+        compile ":asset-pipeline:1.9.9"
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate4:4.3.5.5" // or ":hibernate:3.6.10.17"
+       // runtime ":hibernate4:4.3.5.5" // or ":hibernate:3.6.10.17"
+        runtime ':hibernate4:4.3.6.1'
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
 
